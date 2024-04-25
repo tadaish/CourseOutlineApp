bc77cbe43285a031db8d3dbc82459874a2c3d656
tadaish <tadaishi159@gmail.com>
2024-04-26 00:41:08 +0700

    Add Comment Model

--- courseoutlineapi/courseoutline/models.py
+++ courseoutlineapi/courseoutline/models.py
@@ -64,3 +64,9 @@ class Course(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Comment(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    outline = models.ForeignKey(Outline, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
